c701ebdf3ebc9e38486614748fb4bb885eac4f1e
yangyifanYYF <bbyyszbd@163.com>
2024-12-18 13:52:40 +0800

    feat：准备开始做对称性改动

--- AG-Pose-main/utils/yyf_utils.py
+++ AG-Pose-main/utils/yyf_utils.py
@@ -1,7 +1,7 @@
 import pickle
 
 # 打开.pkl文件并加载数据
-with open('/workspace/code/data/PoseData/Real/train/scene_1/0000_label.pkl', 'rb') as file:  # 'rb' 表示以二进制读取模式打开文件
+with open('/workspace/code/data/PoseData/gts/real_test/results_real_test_scene_1_0000.pkl', 'rb') as file:  # 'rb' 表示以二进制读取模式打开文件
     data = pickle.load(file)
 
 # 打印加载的数据
